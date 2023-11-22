d87874b10fb3669785b676830611eee751eb3a68
Michael P <michpouls@gmail.com>
2023-11-22 12:57:21 +0100

    fwijep

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+const formParent = document.getElementById('main-formular');
+
+const formFirstName = document.getElementById('firstName');
+const formLastName = document.getElementById('familyName');
+const formAddress = document.getElementById('myAddress');
+const formZip = document.getElementById('zipCode');
+const formEmail = document.getElementById('myEmail');
+const formButton = document.getElementById('myButton');
+
+formButton.addEventListener('click', (event)=> {
+    event.preventDefault();
+
+})
